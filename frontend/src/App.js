cb567456765e451c5e4ec43fd8d25cbe2dd78f76
Aman <amangarg0303@gmail.com>
2024-06-19 17:43:38 +0530

    proper sidebar creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,98 @@
 import "./App.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Auth from "./pages/auth/Auth";
+import Dashboard from "./pages/dashboard/Dashboard";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import Analytics from "./pages/analytics/Analytics";
 
 function App() {
+  const currentUser = true;
+
+  const HomeLayout = () => {
+    return (
+      <div className="App">
+        <Outlet />
+      </div>
+    );
+  };
+
+  const DashboardLayout = () => {
+    return (
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            flex: 1,
+            position: "fixed",
+            top: 0,
+            backgroundColor: "white",
+          }}
+        >
+          <Sidebar />
+        </div>
+        <div style={{ flex: 5, marginLeft: "15rem" }}>
+          <Outlet />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          path: "/",
+          element: !currentUser ? (
+            <Auth />
+          ) : (
+            <>
+              <div style={{ display: "flex" }}>
+                <div
+                  style={{
+                    flex: 1,
+                    position: "fixed",
+                    top: 0,
+                    backgroundColor: "white",
+                  }}
+                >
+                  <Sidebar />
+                </div>
+                <div style={{ flex: 5, marginLeft: "15rem" }}>
+                  <Dashboard />
+                </div>
+              </div>
+            </>
+          ),
+        },
+        // {
+        //   path: "/playquiz/:quizId",
+        //   element: <PlayQuiz />,
+        // },
+      ],
+    },
+    {
+      path: "/dashboard",
+      element: currentUser ? <DashboardLayout /> : <HomeLayout />,
+      children: [
+        {
+          path: "/dashboard/",
+          element: currentUser ? <Dashboard /> : <Auth />,
+        },
+        {
+          path: "/dashboard/analytics",
+          element: currentUser ? <Analytics /> : <Auth />,
+        },
+      ],
+    },
+    // {
+    //   path: "/*",
+    //   element: <PageNotFound />,
+    // },
+  ]);
   return (
     <div className="App">
-      <Auth />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./dashboard.module.css";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+      <h2>dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
