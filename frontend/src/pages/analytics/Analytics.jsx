66a91136831a566f1426ca828b87a78baa760848
Aman <amangarg0303@gmail.com>
2024-06-19 18:16:12 +0530

    analytics page

--- frontend/src/pages/analytics/Analytics.jsx
+++ frontend/src/pages/analytics/Analytics.jsx
@@ -1,10 +1,81 @@
 import React from "react";
 import styles from "./analytics.module.css";
+import { GoDotFill } from "react-icons/go";
 
 const Analytics = () => {
   return (
-    <div>
-      <h2>Analytics page</h2>
+    <div className={styles.container}>
+      <h2>Analytics</h2>
+
+      <div className={styles.analyticWrapper}>
+        <div className={styles.analytic}>
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              Backlog Tasks
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              To-do Tasks
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              In-progress Tasks
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              Completed Tasks
+            </p>
+            <strong>16</strong>
+          </div>
+        </div>
+
+        <div className={styles.analytic}>
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              Low Priority
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              Moderate Priority
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              High-priority
+            </p>
+            <strong>16</strong>
+          </div>
+
+          <div className={styles.singleAnalytic}>
+            <p className={styles.titleContent}>
+              <GoDotFill fill="#91c5cc" />
+              Due Date Tasks
+            </p>
+            <strong>16</strong>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,52 +1,31 @@
 import React from "react";
 import styles from "./dashboard.module.css";
+import numToMonth from "../../utils/numToMonth";
 
 const Dashboard = () => {
+  const date = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+
   return (
-    <div>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
-      <h2>dashboard</h2>
+    <div className={styles.container}>
+      <div className={styles.topBar}>
+        <h1 style={{ fontSize: "1.4rem" }}>Welcome! Aman</h1>
+        <p style={{ color: "gray", fontSize: "1.2rem" }}>
+          {date} {numToMonth(month)}, {year}
+        </p>
+      </div>
+
+      <div className={styles.content}>
+        <p style={{ fontSize: "1.6rem", fontWeight: "600" }}>Board</p>
+        <select>
+          <option value="today">Today</option>
+          <option selected value="week">
+            This Week
+          </option>
+          <option value="month">This Month</option>
+        </select>
+      </div>
     </div>
   );
 };
